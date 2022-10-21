2e42ff32c3b2e6117a1ec36215d8295499029aa4
riverplant <riverplant@hotmail.com>
2022-10-20 22:20:03 -0400

    first commit

--- microservice-api/src/main/java/com/niterre/microservice/NiterreApplication.java
+++ microservice-api/src/main/java/com/niterre/microservice/NiterreApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NiterreApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(NiterreApplication.class, args);
 	}
 
